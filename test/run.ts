a6cd6ca946bacb2369752b60ff829bf8ad66d3d2
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-08-14 01:24:22 +0100

    Run equipment and volume tests

--- test/run.ts
+++ test/run.ts
@@ -2,6 +2,7 @@
 import './domain/chemistry.test';
 import './domain/camera-confidence.test';
 import './domain/spoons.test';
+import './domain/equipment.test';
 import './server/mock-spa.test';
 import './server/manual-spa.test';
 import './server/runtime.test';
