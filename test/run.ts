39c11ca17c5c5e5b2b765938e61e42875a1314fb
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-08-29 18:14:14 +0100

    test(ble): run BLE-C600 decoder tests

--- test/run.ts
+++ test/run.ts
@@ -3,6 +3,7 @@ import './domain/chemistry.test';
 import './domain/camera-confidence.test';
 import './domain/spoons.test';
 import './domain/equipment.test';
+import './domain/ble-c600.test';
 import './server/mock-spa.test';
 import './server/manual-spa.test';
 import './server/bubbles.test';
