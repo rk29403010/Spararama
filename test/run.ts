048523e6e7bd07df6e8624784681ca78dcf6d1ee
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-09-06 19:07:51 +0100

    test: run strip scale regression tests

--- test/run.ts
+++ test/run.ts
@@ -5,6 +5,7 @@ import './domain/spoons.test';
 import './domain/equipment.test';
 import './domain/ble-c600.test';
 import './domain/heating.test';
+import './domain/strip-scales.test';
 import './server/mock-spa.test';
 import './server/manual-spa.test';
 import './server/bubbles.test';
