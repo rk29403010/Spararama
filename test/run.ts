7ec8484cd6d7c14e3a69fe3c23024b4bc64e35c5
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-09-11 02:50:53 +0100

    test: run temperature navigation regressions

--- test/run.ts
+++ test/run.ts
@@ -8,6 +8,7 @@ import './domain/heating.test';
 import './domain/strip-scales.test';
 import './lib/spa-snapshot-cache.test';
 import './lib/temperature-chart.test';
+import './lib/temperature-window.test';
 import './server/mock-spa.test';
 import './server/manual-spa.test';
 import './server/bubbles.test';
