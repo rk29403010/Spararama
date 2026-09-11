18d8befbdd950fd56fc5ef7d94f287f9327c1f01
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-09-11 02:03:01 +0100

    test: run temperature chart regressions

--- test/run.ts
+++ test/run.ts
@@ -7,6 +7,7 @@ import './domain/ble-c600.test';
 import './domain/heating.test';
 import './domain/strip-scales.test';
 import './lib/spa-snapshot-cache.test';
+import './lib/temperature-chart.test';
 import './server/mock-spa.test';
 import './server/manual-spa.test';
 import './server/bubbles.test';
