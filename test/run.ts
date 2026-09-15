90956290f7b05f0bf3d212b844f019cedd48cf7a
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-09-15 10:16:44 +0100

    test(telemetry): include pushed-status regression

--- test/run.ts
+++ test/run.ts
@@ -16,6 +16,7 @@ import './server/alexa-direct.test';
 import './server/runtime.test';
 import './server/recovery-bridge.test';
 import './server/telemetry.test';
+import './server/telemetry-push.test';
 import './server/shared-telemetry.test';
 import './server/telemetry-rollup.test';
 import './server/temperature-resolver.test';
