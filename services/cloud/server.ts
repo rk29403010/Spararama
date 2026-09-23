426cef929f9813a5c39a2827bd07504aa2d24166
Robin K <1700692+rk29403010@users.noreply.github.com>
2026-09-23 13:44:17 +0100

    security: use distributed limiter in Cloud Run

--- services/cloud/server.ts
+++ services/cloud/server.ts
@@ -1,14 +1,14 @@
 import 'dotenv/config';
 import { createCloudControlApp } from '../../server/remote/cloud/http';
 import { FirebaseCloudAuthenticator } from '../../server/remote/cloud/firebase-auth';
-import { FirebaseCloudControlStore } from '../../server/remote/cloud/firebase-store';
+import { FirebaseCloudControlStore, FirestoreCommandRateLimiter } from '../../server/remote/cloud/firebase-store';
 import { CloudControlService } from '../../server/remote/cloud/service';
 
 const port = Math.max(1, Number(process.env.PORT || 8080));
 const host = process.env.HOST || '0.0.0.0';
 
 const store = new FirebaseCloudControlStore();
-const service = new CloudControlService(store);
+const service = new CloudControlService(store, { limiter: new FirestoreCommandRateLimiter() });
 const authenticator = new FirebaseCloudAuthenticator();
 const app = createCloudControlApp({ service, authenticator });
 
